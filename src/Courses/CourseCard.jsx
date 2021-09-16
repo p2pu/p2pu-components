e49b28a84fe3918220448765d8bf13b1e6adc288
Dirk Uys <dirkcuys@gmail.com>
2021-09-16 15:59:25 +0200

    Update course card

--- src/Courses/CourseCard.jsx
+++ src/Courses/CourseCard.jsx
@@ -35,6 +35,89 @@ const CourseCard = (props) => {
   }
 
   return (
+    <div className="card col-12 course horizontal in-progress">
+      <div className="status-tag">in progress</div>
+      <div className="row g-0">
+        <div className="col-12 col-md-6 col-lg-12 col-xl-7">
+          <div className="card-header">
+            <h3 className="card-title">{ props.course.title }</h3>
+            <div className="lowbrow row">
+              <div className="stars mb-2 pe-md-0 d-flex col-12 col-md-auto">
+                <i className="material-icons">star</i>
+                <i className="material-icons">star</i>
+                <i className="material-icons">star</i>
+                <i className="material-icons">star</i>
+                <i className="material-icons">star_half</i>
+              </div>
+              <div className="col-12 col-md-auto mb-2">{rating} | {usage}</div>
+            </div>
+          </div>
+          
+          <div className="card-body">
+            <p className="card-text">{props.course.caption}</p>
+          </div>
+        </div>
+    
+        <footer className="card-footer col-12 col-md-6 col-lg-12 col-xl-5">
+          <div className="table-responsive">
+            <table className="table">
+    
+              <tbody>
+                <tr className="topics">
+                  <th scope="row">Topics</th>
+                  <td className="topics-list">
+                    { topicsList.map((topic, index) => <>
+                          {!!index && ', '}
+                          <a key={`topic-${index}`} className="topic" href="">{topic}</a>
+                        </>
+                      )
+                    }
+                  </td>
+                </tr>
+                <tr className="provider">
+                  <th scope="row">{t`Provider`}</th>
+                  <td>{props.course.provider}</td>
+                </tr>
+                { props.course.platform &&
+                  <tr className="platform">
+                    <th scope="row">{t`Platform`}</th>
+                    <td colspan="2">{props.course.platform}</td>
+                  </tr>
+                }
+                <tr className="website">
+                 <th scope="row">{t`Website`}</th>
+                  <td colspan="2">
+                    <a href={props.course.link} target="_blank">{props.course.link}</a>
+                  </td>
+                </tr>
+                <tr className="platform">
+                  <th scope="row">{t`Access`}</th>
+                  <td colspan="2">{availability }</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>	
+          <div className="row justify-content-center justify-content-md-end">
+            {
+              moreInfo &&
+                <a target="_blank" href={props.course.course_page_url} className="btn p2pu-btn btn-sm secondary gray col-sm-auto m-2">{t`More details`}</a>
+            }
+            {
+              courseLink &&
+                <a href={props.course.link} className="btn p2pu-btn btn-sm gray col-sm-auto m-2">Use this course</a>
+            }
+            { 
+              props.onSelectResult &&
+                <a className="btn p2pu-btn btn-sm gray col-sm-auto m-2"  onClick={() => props.onSelectResult(props.course)}>{props.buttonText}</a>
+            }
+          </div>
+        </footer>
+      </div>
+    </div>
+
+  )
+
+  return (
     <Card classes={`${props.classes}`} colorClass={colorClass} id={props.id}>
       <CardTitle>{ props.course.title }</CardTitle>
       <CardBody>
