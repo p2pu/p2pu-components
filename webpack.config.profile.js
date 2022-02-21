0fdccec31d0f89ab54c9bee4f69e9b5e74bf1248
Dirk Uys <dirkcuys@gmail.com>
2022-02-21 17:48:30 +0200

    Update some deps

--- webpack.config.profile.js
+++ webpack.config.profile.js
@@ -2,6 +2,11 @@ const config = require('./webpack.config');
 
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
-config[0].plugins = config[0].plugins.concat([new BundleAnalyzerPlugin({ analyzerPort: 5555 })]);
+config[0].plugins = config[0].plugins.concat([
+  new BundleAnalyzerPlugin({ 
+    analyzerPort: 5555,
+    //analyzerHost: '0.0.0.0',
+  })
+]);
 
 module.exports = config;
