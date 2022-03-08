78512ed25befaa4c101031e0d38855f3342e59c2
Dirk Uys <dirkcuys@gmail.com>
2022-03-08 15:17:20 +0200

    Hardcode Browse component for course search

--- src/Courses/SearchCourses.jsx
+++ src/Courses/SearchCourses.jsx
@@ -5,6 +5,7 @@ import SearchAndFilter from '../Courses/SearchAndFilter'
 import SearchTags from '../Search/SearchTags'
 import SearchSummary from './SearchSummary'
 import DefaultNoResults from '../Search/DefaultNoResults'
+import BrowseCourses from './Browse'
 import { SEARCH_PROPS, OPEN_TAB_TEXT, CLOSED_TAB_TEXT } from '../utils/constants'
 
 
@@ -14,32 +15,25 @@ export default class SearchCourses extends Component {
   }
 
   render() {
-    const extraProps = {
-      filterCollection: SEARCH_PROPS[this.props.searchSubject].filters,
-      sortCollection: SEARCH_PROPS[this.props.searchSubject].sort,
-    }
-
-    const Browse = this.props.Browse;
     return(
       <div className="row">
         <div className="filter-sidebar sidebar sticky-top col-0 col-lg-4 col-xl-3">
-        <SearchAndFilter
-          placeholder= {SEARCH_PROPS[this.props.searchSubject].placeholder}
-          {...extraProps}
-          {...this.props}
-        />
+          <SearchAndFilter
+            placeholder= {SEARCH_PROPS[this.props.searchSubject].placeholder}
+            {...this.props}
+          />
         </div>
         <div className="col-12 col-lg-8 col-xl-9 ps-lg-3 ps-xl-4">
-        <SearchSummary
-          {...this.props}
-        />
-        <Browse
-          {...this.props}
-        />
-        {
-          this.props.isLoading &&
-          <div className="loader" />
-        }
+          <SearchSummary
+            {...this.props}
+          />
+          <BrowseCourses
+            {...this.props}
+          />
+          {
+            this.props.isLoading &&
+              <div className="loader" />
+          }
         </div>
       </div>
     )
